34361f2ec012108d8e1eb6ad6ffbeacbf569ea05
myhency <hency.yeo@gmail.com>
2022-12-15 01:57:15 +0900

    initial commit

---
+++ app/src/main/java/io/play/clean_architecture_boilerplate/MainActivity.kt
@@ -0,0 +1,43 @@
+package io.play.clean_architecture_boilerplate
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import io.play.clean_architecture_boilerplate.ui.theme.CleanarchitectureboilerplateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CleanarchitectureboilerplateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CleanarchitectureboilerplateTheme {
+        Greeting("Android")
+    }
+}
